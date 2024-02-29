6f9e6dd846f164f02646cda597e7f2e9277086e7
Siddhartha Roy <sid86harth@gmail.com>
2024-03-01 02:08:29 +0530

    feat: remove dark mode styling

--- pages/customers.tsx
+++ pages/customers.tsx
@@ -19,15 +19,15 @@ const Customers = () => {
   return (
     <Wrapper name="Cutomers">
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-left text-gray-500 ">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
             <tr className="">
               <th scope="col" className="p-4">
                 <div className="flex items-center">
                   <input
                     id="checkbox-all-search"
                     type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
                   />
                   <label htmlFor="checkbox-all-search" className="sr-only">
                     checkbox
@@ -60,14 +60,14 @@ const Customers = () => {
                 return (
                   <tr
                     key={customer._id}
-                    className="bg-white  border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    className="bg-white  border-b 0  hover:bg-gray-50 "
                   >
                     <td className="w-4 p-4">
                       <div className="flex items-center">
                         <input
                           id="checkbox-table-search-1"
                           type="checkbox"
-                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded "
                         />
                         <label
                           htmlFor="checkbox-table-search-1"
@@ -79,7 +79,7 @@ const Customers = () => {
                     </td>
                     <th
                       scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
                     >
                       {customer.name}
                     </th>
@@ -98,13 +98,13 @@ const Customers = () => {
                     <td className="flex items-center px-6 py-4">
                       <a
                         href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                        className="font-medium text-blue-600  hover:underline"
                       >
                         Edit
                       </a>
                       <a
                         href="#"
-                        className="font-medium text-red-600 dark:text-red-500 hover:underline ms-3"
+                        className="font-medium text-red-600 hover:underline ms-3"
                       >
                         Remove
                       </a>
