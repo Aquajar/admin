f79d1cbe81096065ab149cec0b1b7b4627f8d53e
Siddhartha Roy <sid86harth@gmail.com>
2024-08-01 16:31:46 +0530

    feat: add seo tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,20 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <title>Admin | Aquajar</title>
+        <meta name="description" content="Welcome to the Aquajar Admin Panel" />
+        <meta name="application-name" content="Aquajar" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Aquajar" />
+        <meta name="description" content="Welcome to the Aquajar Admin Panel" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#0199fe" />
+        <link rel="manifest" href="/manifest.json" />
         <Main />
         <NextScript />
       </body>
