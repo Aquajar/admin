d5c70b1ea12a0d1fcebf3580c23200c862cc1e5f
Siddhartha Roy <sid86harth@gmail.com>
2025-03-21 03:47:07 +0530

    fix: module error

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import copy from "copy-to-clipboard";
+import toast from "react-hot-toast";
+
+export const copyTextToKeyboard = (text: string) => {
+  copy(text);
+  toast.success("Copied to clipboard");
+};
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
